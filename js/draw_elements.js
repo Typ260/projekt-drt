22fc0c1ea60945345bd2e1b6a57e4c518960d366
David <Typ260@googlemail.com>
2020-05-04 17:29:26 +0200

    Refactoring

--- js/draw_elements.js
+++ js/draw_elements.js
@@ -1,11 +1,13 @@
-console.log ("Loaded ./draw_elements.js");
+// Sprechblase 1
+var bubble1 = new Image;
+bubble1.src = "./img/blase1.png";
 
 function draw_bubble1(line1, line2) {
 	// Position der Blase an die Position des Spielers anpassen
 	bubbleX = currentX - 97;
 	bubbleY = currentY - 70;
 	// Blase auf Position zeichnen
-	c.drawImage(event1_img, bubbleX, bubbleY);
+	c.drawImage(bubble1, bubbleX, bubbleY);
 	// Blase mit Text füllen 
 	c.fillText(line1,bubbleX + 20 ,bubbleY + 20);
 	c.fillText(line2,bubbleX + 20 ,bubbleY + 37);
