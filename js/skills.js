d2502e5cf5c36d723538a4db5428fcf34b3ef2c4
David <Typ260@googlemail.com>
2020-05-07 08:35:41 +0200

    temporary database for skills
    Goes SQL at some point... maybe.

---
+++ js/skills.js
@@ -0,0 +1,51 @@
+// Alle Skills
+
+// Krieger
+// Slot 1 Möglichkeiten
+var erbitterterSchlag = ["Erbitterter Schlag", 10];
+var axtHieb = ["Axt Hieb", 15];
+var doppelschlag = ["Doppelschlag", 20];
+// Slot 2 Möglichkeiten
+var bogenschuss = ["Bogenschuss", 10];
+var faecherschuss = ["Fächerschuss", 15];
+var explosionsschuss = ["Explosionsschuss", 20];
+// Slot 3 Möglichkeiten
+var speerstich = ["Speerstich", 10];
+var speerwurf = ["Speerwurf", 15];
+var sturmangriff = ["Sturmangriff", 20];
+
+// Magier
+// Slot 1 Möglichkeiten
+var stabSchlag = ["Stab Schlag", 10];
+var stabStoss = ["Stab Stoß", 15];
+var stabWirbel = ["Stab Wirbel", 20];
+// Slot 2 Möglichkeiten
+var feuerball = ["Feuerball", 10];
+var blitzschlag = ["Blitzschlag", 15];
+var flammenwelle = ["Flammenwelle", 20];
+// Slot 3 Möglichkeiten
+var illusion = ["Illusion erschaffen", 10];
+var dornenranken = ["Dornenranken erschaffen", 15];
+var teleport = ["Teleport Angriff", 20];
+
+// Schurke
+// Slot 1 Möglichkeiten
+var dolchstich = ["Dolchstich", 10];
+var dolchwurf = ["Dolchwurf", 15];
+var dolchfaecher = ["Dolchfächer", 20];
+// Slot 2 Möglichkeiten
+var armbrustschuss = ["Armbrust Schuss", 10];
+var wurfmesser = ["Wurfmesser", 15];
+var giftpfeil = ["Gift Pfeil", 20];
+// Slot 3 Möglichkeiten
+var schattenangriff = ["Schattenangriff", 10];
+var sprungstoß = ["Sprung Stoß", 15];
+var rauchbombe = ["Rauchbombe", 20];
+
+
+
+
+// Slot 4 Möglichkeiten - Klassenübergreifend
+var kleinerHeiltrank = ["Kleiner Heiltrank", 10];
+var grosserHeiltrank = ["Großer Heiltrank", 20];
+var magischeSalbe = ["Magische Salbe",  30];
